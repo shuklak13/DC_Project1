bb4ea44a37b75d952104c1b319cebd4cbcb7c46a
Karan Shukla <kxs141930@csgrads1.utdallas.edu>
2018-02-28 18:32:58 -0600

    Launcher based on jar

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -10,11 +10,13 @@ PROJDIR=$HOME/DC_Project1
 CONFIGLOCAL=$HOME/DC_Project1/config.txt
 
 # Directory your java classes are in
-javac=$HOME/DC_Project1/src/dc_project1/*.java
+javac $HOME/DC_Project1/src/dc_project1/*.java
 BINDIR=$HOME/DC_Project1/src/dc_project1
 
 # Your main project class
 PROG=DC_Project1
+
+JAR=$HOME/DC_Project1/dist/DC_Project1.jar
 
 n=6
 
@@ -27,11 +29,10 @@ cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
     do
     	echo "do loop"
     	read line
-    	p=$( echo $line | awk '{ print $1 }' )
+    	node=$( echo $line | awk '{ print $1 }' )
         host=$( echo $line | awk '{ print $2 }' )
 	
-	# gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $p; $SHELL" &
-	# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $p; $SHELL
+	gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -jar $JAR $node; $SHELL" &
 
         n=$(( n - 1 ))
     done
