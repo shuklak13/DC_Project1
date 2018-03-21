b8534341a57e7b428115ef14c5cc23d68d0b6c52
Karan Shukla <kxs141930@dc08.utdallas.edu>
2018-03-21 18:17:57 -0500

    Fix cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,26 +6,23 @@ netid=kxs141930
 
 # Root directory of your project
 PROJDIR=$HOME/DC_Project1
-
-# Directory where the config file is located on your local system
-CONFIGLOCAL=$HOME/DC_Project1/config/config.txt
-
-n=6
+CONFIGLOCAL=$PROJDIR/config/config2copy.txt
 
 cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
 (
     read i
     echo $i
-    while [[ $n -gt 0 ]]
+    while [[ $i -gt 0 ]]
     do
     	read line
         host=$( echo $line | awk '{ print $2 }' )
 
+	echo $i
         echo $host
         gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host killall -u $netid" &
         sleep 1
 
-        n=$(( n - 1 ))
+        i=$(( i - 1 ))
     done
    
 )
