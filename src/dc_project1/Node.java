9309410d2bfc6a20df5f5795665d33109ea0e28e
Karan Shukla <shuklak13@gmail.com>
2018-03-22 13:26:50 -0500

    print once BFS begins

--- src/dc_project1/Node.java
+++ src/dc_project1/Node.java
@@ -161,6 +161,7 @@ class Node{
     }
     
     public void initiateBfs(){
+      System.out.println("Beginning BFS; leader is " + leader);
       synchronized(this){
         if(b1==null)
           b1 = new Bfs(isLeader(), neighbors, this);
